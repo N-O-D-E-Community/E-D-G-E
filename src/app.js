6f099e0fa27a89154fc157f871aef7f458699490
lukas2005 <lukas2005.38@gmail.com>
2018-04-27 17:50:13 +0200

    reformatted this a bit

--- src/app.js
+++ src/app.js
@@ -16,25 +16,24 @@ for (const file of cmdFiles) {
 
 // events
 client.on('ready', () => {
-  console.log("OK!");
+	console.log("OK!");
 });
 
 client.on('message', msg => {
-  if (!msg.content.startsWith(config.prefix) || msg.author.bot) return;
+	if (!msg.content.startsWith(config.prefix) || msg.author.bot) return;
 
-  // parses command arguments
+	// parses command arguments
 	const args = msg.content.slice(config.prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 
 	if (!client.commands.has(command)){
-    msg.reply("There is no such command!");
-    return;
-  }
+		msg.reply("There is no such command!");
+		return;
+	}
 
 	try {
 		client.commands.get(command).execute(msg, args);
-	}
-	catch (error) {
+	} catch (error) {
 		console.error(error);
 		msg.reply('Error :/');
 	}
