70c881ea72ee5db76a938b92af9e8eb391615eed
Samuel Fichna <d3add3d@gmail.com>
2018-04-28 17:01:13 +0200

    Added link checking (checks if the provided link adheres to the RFC 3986 and checks for HTTP(S) response)

--- src/cmd/link.js
+++ src/cmd/link.js
@@ -1,24 +1,37 @@
+const validUrl = require('valid-url');
+const urlExists = require('url-exists-deep');
+
 module.exports = {
     name: 'link',
     description: 'Checks and adds link to the database',
     execute(refs, msg, args) {
-        let collectionRef = refs.database.collection('links');
-        let query = collectionRef.where('link', '==', args[0]).get().then(snapshot => {
-            let doesExist = false;
-            snapshot.forEach(doc => {
-                if(doc.exists) doesExist = true;
-            })
-            if(doesExist) {
-                msg.reply('Duplicate link, not adding.');
-            } else {
-                msg.reply('Adding link!');
-                collectionRef.add({
-                    'link': args[0],
-                    'timestamp': Date.now(),
-                    'seen': false
-                })
-            }
-        });
+        if(validUrl.isUri(args[0])) {
+            urlExists(args[0]).then(function(response) {
+                if(response) {
+                    let collectionRef = refs.database.collection('links');
+                    collectionRef.where('link', '==', args[0]).get().then(snapshot => {
+                        let doesExist = false;
+                        snapshot.forEach(doc => {
+                            if (doc.exists) doesExist = true;
+                        });
+                        if (doesExist) {
+                            msg.reply('duplicate link, not adding.');
+                        } else {
+                            msg.reply('adding link!');
+                            collectionRef.add({
+                                'link': args[0],
+                                'timestamp': Date.now(),
+                                'seen': false
+                            });
+                        }
+                    });
+                } else {
+                    msg.reply("link is not reachable!")
+                }
+            });
+        } else {
+            msg.reply("link is in invalid format!");
+        }
     }
 
 };
