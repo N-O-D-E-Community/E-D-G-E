7084092ce055aa357d8f006acbcab4e3b8dcf40c
Samuel Fichna <d3add3d@gmail.com>
2018-04-29 16:44:09 +0200

    Help fixed

--- src/cmd/help.js
+++ src/cmd/help.js
@@ -5,13 +5,11 @@ module.exports = {
     description: 'Lists all commands',
     execute(refs, msg, args) {
         let helpString = "```\nAvailable commands:\n";
-        //global.winston.debug(refs.commands);
         refs.commands.forEach((value, key, map) => {
             global.winston.debug('key: ' + key);
-            //global.winston.silly(value);
-            helpString.concat('\n',refs.config.discord.prefix,value['name'],'\t\t',value['description']);
+            helpString = helpString.concat('\n',refs.config.discord.prefix,value['name'],' -- ',value['description']);
         });
-        helpString.concat('\n```');
+        helpString = helpString.concat('\n```');
         msg.channel.send(helpString);
     }
 };
