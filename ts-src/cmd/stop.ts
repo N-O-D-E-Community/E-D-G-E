7eec7afc6a876c827ba6420d111b93c7cabf6c5a
d3add3d <d3add3d@gmail.com>
2018-05-14 21:37:07 +0200

    Initial TypeScript

---
+++ ts-src/cmd/stop.ts
@@ -0,0 +1,16 @@
+/* Author: D3add3d */
+
+module.exports = {
+    name: 'stop',
+    description: 'Stops the bot',
+    type: 2,
+    execute(refs, msg, args) {
+        refs.winston.debug("Stop command executed by: ", msg.author.username);
+        msg.channel.send("Stopping.");
+        refs.winston.info("Stopping.");
+        setTimeout(() => {
+            refs.client.destroy();
+            process.exit(0)
+        }, 1000);
+    }
+};
